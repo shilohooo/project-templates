cc5144c6d4f7a7e570e8c2e4b617da9576071697
shiloh <lixiaolei595@gmail.com>
2025-04-09 17:51:43 +0800

    initial commit

---
+++ backend/java/jdk21-maven/src/main/java/com/github/shiloh/App.java
@@ -0,0 +1,13 @@
+package com.github.shiloh;
+
+/**
+ * Main entry
+ *
+ * @author shiloh
+ * @date 2025/4/9 17:34
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
